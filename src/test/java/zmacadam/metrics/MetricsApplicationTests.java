e00a7b1ea751d682ddc0852885730ec7f2f39354
Zach Macadam <z.macadam@tcu.edu>
2020-12-18 10:57:44 -0800

    Initialized project

---
+++ src/test/java/zmacadam/metrics/MetricsApplicationTests.java
@@ -0,0 +1,13 @@
+package zmacadam.metrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetricsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
